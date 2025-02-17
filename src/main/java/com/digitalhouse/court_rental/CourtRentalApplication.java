7d50d5b123af92483164497b3dc2c8b2ee81aacf
LinaMVG <limarcegarzon@gmail.com>
2025-02-16 20:14:58 -0500

    Initial commit

---
+++ src/main/java/com/digitalhouse/court_rental/CourtRentalApplication.java
@@ -0,0 +1,13 @@
+package com.digitalhouse.court_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalhouse/court_rental/CourtRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalhouse.court_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtRentalApplicationTests {
+
+	//@Test
+	//void contextLoads() {
+	//}
+
+}

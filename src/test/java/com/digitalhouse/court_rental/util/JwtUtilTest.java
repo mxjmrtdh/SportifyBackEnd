88def4f869c485aca1dd877aebd04ceda668b4c1
LinaMVG <limarcegarzon@gmail.com>
2025-03-04 22:49:51 -0500

    delete import unused in test

--- src/test/java/com/digitalhouse/court_rental/util/JwtUtilTest.java
+++ src/test/java/com/digitalhouse/court_rental/util/JwtUtilTest.java
@@ -1,31 +1,17 @@
 package com.digitalhouse.court_rental.util;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-
-import java.security.Key;
-import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mockStatic;
 
 class JwtUtilTest {
 
     private JwtUtil jwtUtil;
-    private Key secretKey;
     private String testToken;
 
     @BeforeEach
     void setUp() {
-        secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
         jwtUtil = new JwtUtil();
         testToken = jwtUtil.generateToken("test@example.com", "ROLE_USER");
     }

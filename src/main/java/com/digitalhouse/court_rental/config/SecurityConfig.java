da1362911ad0f83f30180a82d5a5e0fa18b6cb23
LinaMVG <limarcegarzon@gmail.com>
2025-03-15 21:57:01 -0500

    feat:: modify access, any user can make queries

---
+++ src/main/java/com/digitalhouse/court_rental/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.digitalhouse.court_rental.config;
+
+import com.digitalhouse.court_rental.util.JwtAuthFilter;
+import com.digitalhouse.court_rental.util.JwtUtil;
+import jakarta.servlet.Filter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtUtil jwtUtil, UserDetailsService userDetailsService) throws Exception {
+        http
+                .cors(cors -> cors.configure(http)) // Habilita CORS en Spring Security
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**", "/api/public/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/user/**").hasAnyRole("SUPER_ADMIN", "ADMIN")
+                        .requestMatchers("/api/roles/**").hasAnyRole("SUPER_ADMIN", "ADMIN")
+                        .requestMatchers("/api/bookings/**").hasAnyRole("SUPER_ADMIN", "ADMIN", "USER")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthFilter(jwtUtil, userDetailsService), UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}

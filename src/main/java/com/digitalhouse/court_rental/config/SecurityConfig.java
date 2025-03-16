9eaca6a202948948f2f49b04b5e2566af2d71752
LinaMVG <limarcegarzon@gmail.com>
2025-03-15 21:57:01 -0500

    feat:: modify access, any user can make queries

--- src/main/java/com/digitalhouse/court_rental/config/SecurityConfig.java
+++ src/main/java/com/digitalhouse/court_rental/config/SecurityConfig.java
@@ -32,11 +32,11 @@ public class SecurityConfig {
                 .cors(cors -> cors.configure(http)) // Habilita CORS en Spring Security
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/api/public/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/api/public/**", "api/bookings/search").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/user/**").hasAnyRole("SUPER_ADMIN", "ADMIN")
                         .requestMatchers("/api/roles/**").hasAnyRole("SUPER_ADMIN", "ADMIN")
-                        .requestMatchers("/api/bookings/**").hasAnyRole("SUPER_ADMIN", "ADMIN", "USER")
+                        .requestMatchers("/api/bookings/create").hasAnyRole("SUPER_ADMIN", "ADMIN", "USER")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthFilter(jwtUtil, userDetailsService), UsernamePasswordAuthenticationFilter.class)

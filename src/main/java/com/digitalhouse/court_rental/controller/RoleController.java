cf581fc1d9d6136069154ab8a3bf91adceaa0f55
LinaMVG <limarcegarzon@gmail.com>
2025-03-23 20:38:08 -0500

    change port to 3000

--- src/main/java/com/digitalhouse/court_rental/controller/RoleController.java
+++ src/main/java/com/digitalhouse/court_rental/controller/RoleController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/roles")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
 public class RoleController {
     private final RoleService roleService;
 

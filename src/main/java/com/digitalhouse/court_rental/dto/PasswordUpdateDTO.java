e70ccf4c7d772a2784323c58f03edd81e1a5d28c
LinaMVG <limarcegarzon@gmail.com>
2025-03-28 21:36:52 -0500

    add endpoint to updatepassword

---
+++ src/main/java/com/digitalhouse/court_rental/dto/PasswordUpdateDTO.java
@@ -0,0 +1,17 @@
+package com.digitalhouse.court_rental.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PasswordUpdateDTO {
+    @NotBlank(message = "Old password is required")
+    private String oldPassword;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 6, message = "New password must be at least 6 characters long")
+    private String newPassword;
+}

47bc7d38a908eddb36c29d2a1a58c38fd2a4f659
LinaMVG <limarcegarzon@gmail.com>
2025-03-28 21:36:52 -0500

    add endpoint to updatepassword

--- src/main/java/com/digitalhouse/court_rental/controller/UserController.java
+++ src/main/java/com/digitalhouse/court_rental/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.digitalhouse.court_rental.controller;
 
+import com.digitalhouse.court_rental.dto.PasswordUpdateDTO;
 import com.digitalhouse.court_rental.dto.UserDTO;
 //import com.digitalhouse.court_rental.service.UserService;
 import com.digitalhouse.court_rental.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +32,18 @@ public class UserController {
     public ResponseEntity<UserDTO> getCurrentUser(@AuthenticationPrincipal UserDetails userDetails) {
         UserDTO userDTO = userService.getCurrentUser(userDetails.getUsername());
         return ResponseEntity.ok(userDTO);
+    }
+
+    @PutMapping("/update-password")
+    public ResponseEntity<String> updatePassword(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestBody PasswordUpdateDTO passwordUpdateDTO) {
+
+        try {
+            userService.updatePassword(userDetails.getUsername(), passwordUpdateDTO);
+            return ResponseEntity.ok("Password updated successfully");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 }
--- src/main/java/com/digitalhouse/court_rental/service/UserService.java
+++ src/main/java/com/digitalhouse/court_rental/service/UserService.java
@@ -1,11 +1,15 @@
 package com.digitalhouse.court_rental.service;
 
+import com.digitalhouse.court_rental.dto.PasswordUpdateDTO;
 import com.digitalhouse.court_rental.dto.UserDTO;
 import com.digitalhouse.court_rental.entity.User;
 import com.digitalhouse.court_rental.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,6 +18,9 @@ import java.util.stream.Collectors;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public List<UserDTO> getAllUsers() {
         List<User> users = userRepository.findAll();
@@ -25,5 +32,18 @@ public class UserService {
                 .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
 
         return new UserDTO(user);
+    }
+
+
+    public void updatePassword(String email, PasswordUpdateDTO passwordUpdateDTO) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        if (!passwordEncoder.matches(passwordUpdateDTO.getOldPassword(), user.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Old password is incorrect");
+        }
+
+        user.setPassword(passwordEncoder.encode(passwordUpdateDTO.getNewPassword()));
+        userRepository.save(user);
     }
 }

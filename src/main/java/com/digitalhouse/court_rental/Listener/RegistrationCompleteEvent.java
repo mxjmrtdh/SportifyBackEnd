c06c9354b1778786f528755ad5257040353121c2
LinaMVG <limarcegarzon@gmail.com>
2025-04-03 22:07:38 -0500

    fix linter all repo

--- src/main/java/com/digitalhouse/court_rental/Listener/RegistrationCompleteEvent.java
+++ src/main/java/com/digitalhouse/court_rental/Listener/RegistrationCompleteEvent.java
@@ -1,12 +1,15 @@
 package com.digitalhouse.court_rental.Listener;
 
 import com.digitalhouse.court_rental.entity.User;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.context.ApplicationEvent;
 
+@Setter
+@Getter
 public class RegistrationCompleteEvent extends ApplicationEvent {
-    // Usuario recién registrado.
+
     private User user;
-    // URL base de la aplicación, utilizada para generar enlaces de verificación.
     private String applicationUrl;
 
     // Constructor que inicializa el evento con el usuario y la URL de la aplicación.
@@ -16,23 +19,4 @@ public class RegistrationCompleteEvent extends ApplicationEvent {
         this.applicationUrl = applicationUrl; // Asigna la URL de la aplicación.
     }
 
-    // Método para obtener el usuario asociado al evento.
-    public User getUser() {
-        return user; // Retorna el usuario asociado.
-    }
-
-    // Método para establecer el usuario asociado al evento.
-    public void setUser(User user) {
-        this.user = user; // Asigna el nuevo usuario.
-    }
-
-    // Método para obtener la URL base de la aplicación.
-    public String getApplicationUrl() {
-        return applicationUrl; // Retorna la URL de la aplicación.
-    }
-
-    // Método para establecer la URL base de la aplicación.
-    public void setApplicationUrl(String applicationUrl) {
-        this.applicationUrl = applicationUrl; // Asigna la nueva URL de la aplicación.
-    }
 }
